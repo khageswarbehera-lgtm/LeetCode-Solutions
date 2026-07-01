66735cb8a3bcfcecbae293fff4b29f129265f62b
Mr. Khageswar Behera <khageswarbehera8984@gmail.com>
2026-07-01 09:12:39 +0530

    2050-COUNT-GOOD-NUMBERS Stats: Time: 0 ms (100%), Space: 7.8 MB (97.22%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
+
     const long long MOD = 1e9 + 7;
 
     long long power(long long x, long long n) {
-        long long res = 1;
+
+        long long ans = 1;
+
         while (n > 0) {
-            if (n & 1) res = (res * x) % MOD;
+
+            if (n % 2 == 1)
+                ans = (ans * x) % MOD;
+
             x = (x * x) % MOD;
-            n >>= 1;
+
+            n /= 2;
         }
-        return res;
+
+        return ans;
     }
 
     int countGoodNumbers(long long n) {
+
         long long even = (n + 1) / 2;
+
         long long odd = n / 2;
 
         return (power(5, even) * power(4, odd)) % MOD;
