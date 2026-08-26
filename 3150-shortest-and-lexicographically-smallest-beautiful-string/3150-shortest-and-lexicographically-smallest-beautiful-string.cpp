d87dbbdfb35a77a716e8ea2012c5c99c8e0f451b
Mr. Khageswar Behera <khageswarbehera8984@gmail.com>
2026-08-26 21:47:28 +0530

    3150-SHORTEST-AND-LEXICOGRAPHICALLY-SMALLEST-BEAUTIFUL-STRING Stats: Time: 0 ms (100%), Space: 8.7 MB (82.87%) - LeetHub

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/3150-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+
+        int left = 0;
+        int ones = 0;
+
+        string ans = "";
+
+        for(int right = 0; right < s.size(); right++)
+        {
+            if(s[right] == '1')
+                ones++;
+
+            // Too many 1s
+            while(ones > k)
+            {
+                if(s[left] == '1')
+                    ones--;
+
+                left++;
+            }
+
+            // Exactly k ones
+            if(ones == k)
+            {
+                // Remove unnecessary leading zeros
+                while(left <= right && s[left] == '0')
+                    left++;
+
+                string curr = s.substr(left, right - left + 1);
+
+                if(ans.empty() ||
+                   curr.size() < ans.size() ||
+                   (curr.size() == ans.size() && curr < ans))
+                {
+                    ans = curr;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
