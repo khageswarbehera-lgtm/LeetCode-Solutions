b8ac168b763546a4b63558b093075f905d0e9d04
Mr. Khageswar Behera <khageswarbehera8984@gmail.com>
2026-09-08 23:48:42 +0530

    0165-COMPARE-VERSION-NUMBERS Stats: Time: 0 ms (100%), Space: 8.2 MB (60.62%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -13,26 +13,22 @@ public:
             int num1 = 0;
             int num2 = 0;
 
-            // Convert revision of version1 to number
             while (i < n1 && version1[i] != '.') {
                 num1 = num1 * 10 + (version1[i] - '0');
                 i++;
             }
 
-            // Convert revision of version2 to number
             while (j < n2 && version2[j] != '.') {
                 num2 = num2 * 10 + (version2[j] - '0');
                 j++;
             }
 
-            // Compare revisions
             if (num1 < num2)
                 return -1;
 
             if (num1 > num2)
                 return 1;
 
-            // Skip '.'
             i++;
             j++;
         }
